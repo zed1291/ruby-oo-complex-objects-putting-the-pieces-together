46cabc7c822ae7a1d896ce66e2a6de4a758e5a89
“zed” <zed@kolk.me>
2021-01-12 16:47:49 -0600

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+class Book
+    def initialize title=nil, author=nil, page_count=nil, genre=nil
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
 
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
